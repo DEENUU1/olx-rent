a0d05d1111b5420956bac9e160fd8ab3e16af3eb
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-10 18:10:37 +0100

    Add function to get all xlsx files and display dataframe

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 import streamlit as st
 
+import utils
+
 st.set_page_config(
     page_title="OLX rent",
     page_icon="icon.png"
 )
 st.title("OLX rent")
+
+option = st.selectbox(
+    "Choose file",
+    (utils.get_file_names_startswith_olx())
+)
+
+if option:
+    st.header(f"You choose {option} file")
+    df = utils.get_dataframe_from_file(option)
+    st.dataframe(df)
